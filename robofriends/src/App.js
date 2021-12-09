1fc07e1500c27d0267a4d9f0b7a1854da2b9315f
Ivaylo Ivanov <ivaylozivanov1@gmail.com>
2021-12-09 13:13:16 +0200

    styling

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,30 +1,39 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import ScrollComponent from "./ScrollComponent";
+import "./style/App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
 
+  async componentDidMount() {
+    const data = await (
+      await fetch("https://jsonplaceholder.cypress.io/users")
+    ).json();
+    this.setState({ robots: data });
+  }
+
   onSearchChange = (e) => {
     this.setState({ searchfield: e.target.value });
+  };
+  render() {
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name.toLowerCase().includes(this.state.searchfield);
     });
-    console.log(filteredRobots);
-  };
-  render() {
     return (
       <div className="tc">
-        <h1>Robofriends</h1>
+        <h1 className="f1">Robofriends</h1>
         <SearchBox SearchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <ScrollComponent>
+          <CardList robots={filteredRobots} />
+        </ScrollComponent>
       </div>
     );
   }
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import "./index.css";
+import "./style/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
